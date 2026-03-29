166a25c0da86da99f917bfcd34b459ab7da1a606
ansh-2004 <ansh1295.be22@chitkara.edu.in>
2026-03-29 22:58:42 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import http from 'http'
-import {Server} from 'server.io'
+import {Server} from 'socket.io'
 
 dotenv.config()
+
+// import authRoutes from './routes/authRoutes.js'
+// import videoRoutes from './routes/videoRoutes.js'
+
 
 const app = express()
 const server = http.createServer(app)
@@ -19,3 +23,17 @@ export const io = new Server(server,{
 
 app.use(cors())
 app.use(express.json())
+app.use('/uploads',express.static("uploads"))
+
+// app.use('/api/auth',authRoutes)
+// app.use('/api/videos',videoRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB connected")
+    server.listen(3000,()=> console.log(`Server running on port 3000`))
+})
+.catch((err)=> {
+    console.log(err)
+})
+
